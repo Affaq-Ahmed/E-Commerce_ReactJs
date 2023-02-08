57e71527a1ac012e418dbdd3f887aa205fe40c4c
AffaqAhmed <affaq.ahmed@tintash.com>
2023-02-08 20:21:30 +0500

    AddFirebaseAuthRedirect
    This code contains the code to do the authentication through GOogle Redirect. This code will be removed in the next Commit. Just for reference.

--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,18 +1,36 @@
+import { getRedirectResult } from 'firebase/auth';
+import { useEffect } from 'react';
 import {
+	auth,
 	signInWithGooglePopup,
+	signInWithGoogleRedirect,
 	createUserDocumentAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+	useEffect(() => {
+		const logUser = async () => {
+			const response = await getRedirectResult(auth);
+			if (response) {
+				const userDocRef = await createUserDocumentAuth(response.user);
+			}
+			console.log(response);
+		};
+		logUser();
+	}, []);
+
 	const logGoogleUser = async () => {
 		const response = await signInWithGooglePopup();
-		const createUserDocumentAuth(response.user);
+		const userDocRef = await createUserDocumentAuth(response.user);
 	};
 
 	return (
 		<div>
 			<h1>Sign In Page</h1>
 			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
+			<button onClick={signInWithGoogleRedirect}>
+				Sign in with Google Redirect
+			</button>
 		</div>
 	);
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -25,6 +25,8 @@ provider.setCustomParameters({ prompt: 'select_account' });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+	signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
