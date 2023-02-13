05a9453fc897ff96fee8196fa5c3eb28b2cc75c4
Affaq-Ahmed <affaqahmed165@gmail.com>
2023-02-13 15:26:53 +0500

    ConfigureReduxStore

--- src/store/store.js
+++ src/store/store.js
@@ -1,3 +1,12 @@
-import {} from 'redux'
-import logger from 'redux-logger'
+import { applyMiddleware, compose, configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import { rootReducer } from './root-reducer';
 
+const middlewares = [logger];
+
+const enhancers = compose(applyMiddleware(...middlewares));
+
+export const store = configureStore({
+	reducer: rootReducer,
+	middleware: enhancers,
+});
