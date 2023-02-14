1cf108642ffa50c5e4bafad0aec1c2b20df37eb5
Affaq-Ahmed <affaqahmed165@gmail.com>
2023-02-14 23:21:37 +0500

    MoveLoggerFunction

--- src/store/store.js
+++ src/store/store.js
@@ -1,31 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-	if (!action.type) {
-		return next(action);
-	}
-
-	console.log(
-		'type: ',
-		action.type,
-		'\npayload: ',
-		action.payload,
-		'\nstate: ',
-		store.getState()
-	);
-
-	next(action);
-
-	console.log('next state: ', store.getState());
+const persistConfig = {
+	key: 'root',
+	storage,
+	blacklist: ['user'],
 };
 
-const middlewares = [loggerMiddleware, logger];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middlewares = [process.env.NODE_ENV === 'development' && logger].filter(
+	Boolean
+);
 
 const enhancers = [...middlewares];
 
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
 	middleware: enhancers,
 });
+
+export const persistor = persistStore(store);
