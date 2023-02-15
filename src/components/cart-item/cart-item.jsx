0076c4dbcc7c87cb4d04a76f4a1bf917d7f17f82
Affaq-Ahmed <affaqahmed165@gmail.com>
2023-02-15 19:55:06 +0500

    BranchSeparation
    This branch carries the code for Redux-thunk. Thunk Code was removed after this commit in main branch.

--- src/components/cart-item/cart-item.jsx
+++ src/components/cart-item/cart-item.jsx
@@ -7,7 +7,7 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
 			<div className='item-details'>
 				<span className='name'>{name}</span>
 				<span className='price'>
-					{quantity} x Rs. {price}0
+					{quantity} x $ {price}
 				</span>
 			</div>
 		</div>
