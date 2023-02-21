434eca565e274787c9d6c9b5321d47029e09ea10
Affaq-Ahmed <affaqahmed165@gmail.com>
2023-02-21 22:44:27 +0500

    Feat: AddSVGTypescript

--- src/components/cart-icon/cart-icon.tsx
+++ src/components/cart-icon/cart-icon.tsx
@@ -6,6 +6,7 @@ import {
 	selectIsCartOpen,
 } from '../../store/cart/cart.selector';
 import { setIsCartOpen } from '../../store/cart/cart.action';
+import React from 'react';
 
 const CartIcon = () => {
 	const dispatch = useDispatch();
--- src/routes/navigation/navigation.tsx
+++ src/routes/navigation/navigation.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, Outlet } from 'react-router-dom';
